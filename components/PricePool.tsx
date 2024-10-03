741aa65466da1c30cab871cb82daeab2d79e487a
Mikaël Ruffieux <mikael@redoxdigital.ch>
2024-10-03 14:35:45 +0200

    content: new prices images

--- components/PricePool.tsx
+++ components/PricePool.tsx
@@ -7,9 +7,13 @@ type Props = {
 
 export default function PricePool({ alt }: Props) {
   const prices = [
-    { img: '/layouts/polo1.webp', title: 'Price Title', desc: 'Price Description' },
-    { img: '/layouts/polo2.webp', title: 'Price Title', desc: 'Price Description' },
-    { img: '/layouts/hat.webp', title: 'Price Title', desc: 'Price Description' },
+    {
+      img: '/layouts/WGC-EXCR_price_1000.png',
+      title: '1,000 $ GIFT CARD',
+      desc: 'Price Description',
+    },
+    { img: '/layouts/WGC-EXCR_price_500.png', title: '500 $ GIFT CARD', desc: 'Price Description' },
+    { img: '/layouts/WGC-EXCR_price_250.png', title: '250 $ GIFT CARD', desc: 'Price Description' },
   ];
   return (
     <section className={`${css.pricePool} ${alt && css.alt}`}>
