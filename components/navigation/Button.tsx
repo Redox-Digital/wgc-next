29ed71d0517fbae9c0defcb1b9fbfaa7a5404311
Mikaël Ruffieux <mikael@redoxdigital.ch>
2024-11-28 12:55:34 +0100

    Button : small design fixes

--- components/navigation/Button.tsx
+++ components/navigation/Button.tsx
@@ -5,17 +5,29 @@ type Props = {
   to: string;
   wgc?: boolean;
   white?: boolean;
+  small?: boolean;
   addClass?: string;
   blank?: boolean;
   onClick?: () => void;
   children?: string;
 };
 
-export default function Button({ to, children, wgc, white, addClass, blank, onClick }: Props) {
+export default function Button({
+  to,
+  children,
+  wgc,
+  white,
+  addClass,
+  blank,
+  small,
+  onClick,
+}: Props) {
   return (
     <Link
       href={to}
-      className={`${css.btn} ${wgc && css.btnWgc} ${white && css.white} ${addClass}`}
+      className={`${css.btn} ${wgc && css.btnWgc} ${small && css.small} ${
+        white && css.white
+      } ${addClass}`}
       target={blank ? '_blank' : ''}
       onClick={onClick}
     >
