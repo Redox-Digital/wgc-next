5266083e5f99203030a068ab7dc11a38af0fce71
Mikaël Ruffieux <mikael@redoxdigital.ch>
2024-12-02 13:30:34 +0100

    style: starting Challenge Preview mobile

---
+++ components/content/ChallengePreview.tsx
@@ -0,0 +1,60 @@
+import Button from '../navigation/Button';
+import css from './ChallengePreview.module.scss';
+import Image from 'next/image';
+
+export default function ChallengePreview({
+  id,
+  img,
+  hcp,
+  name,
+  pool,
+  gameType,
+  players,
+  dates,
+  fee,
+}: ChallengePreview) {
+  return (
+    <>
+      <div className={css.challengeDesktop}>
+        <span className={css.id}>#{id}</span>
+        <span className={css.img}>
+          {img && <Image src={img} alt={''} width={45} height={45} />}
+        </span>
+        <span className={css.name}>{name}</span>
+        <span className={css.hcp}>{hcp}</span>
+        <span className={css.pool}>{pool}$</span>
+        <span className={css.gameType}>{gameType}</span>
+        <span className={css.players}>
+          {players}/10 <Image src={'/pictograms/user.svg'} alt={''} width={15} height={15} />
+        </span>
+        <span className={css.dates}>{dates}</span>
+        <span className={css.btn}>
+          <Button small to={'/lobby'}>
+            {fee ? `Buy-in ${fee}$` : 'Join for free'}
+          </Button>
+        </span>
+      </div>
+      <details className={css.challengeMobile}>
+        <summary>
+          {img ? <Image src={img} alt={''} width={45} height={45} /> : <h6>#{id}</h6>}
+          <span className={css.name}>{name}</span>
+          <span className={css.players}>
+            {players}/10 <Image src={'/pictograms/user.svg'} alt={''} width={15} height={15} />
+          </span>
+          <span className={css.pool}>{pool}$</span>
+        </summary>
+        <div className={css.body}>
+          <span className={css.id}>#{id}</span>
+          <span className={css.hcp}>{hcp}</span>
+          <span className={css.gameType}>{gameType}</span>
+          <span className={css.dates}>{dates}</span>
+          <span className={css.btn}>
+            <Button small to={'/lobby'}>
+              {fee ? `Buy-in ${fee}$` : 'Join for free'}
+            </Button>
+          </span>
+        </div>
+      </details>
+    </>
+  );
+}
--- components/layouts/ChallengesSection.tsx
+++ components/layouts/ChallengesSection.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import Button from '../navigation/Button';
 import SectionTitle from './SectionTitle';
 import { SelectInput } from '@/pages/create-private';
+import ChallengePreview from '../content/ChallengePreview';
 
 export default function ChallengeSection() {
   const [showPrivate, setShowPrivate] = useState<boolean>(false);
@@ -174,41 +175,5 @@ export default function ChallengeSection() {
         </div>
       )}
     </section>
-  );
-}
-
-export function ChallengePreview({
-  id,
-  img,
-  hcp,
-  name,
-  pool,
-  gameType,
-  players,
-  dates,
-  fee,
-}: ChallengePreview) {
-  return (
-    <>
-      <div className={css.challenge}>
-        <span className={css.id}>#{id}</span>
-        <span className={css.img}>
-          {img && <Image src={img} alt={''} width={45} height={45} />}
-        </span>
-        <span className={css.name}>{name}</span>
-        <span className={css.hcp}>{hcp}</span>
-        <span className={css.pool}>{pool}$</span>
-        <span className={css.gameType}>{gameType}</span>
-        <span className={css.players}>
-          {players}/10 <Image src={'/pictograms/user.svg'} alt={''} width={15} height={15} />
-        </span>
-        <span className={css.dates}>{dates}</span>
-        <span className={css.btn}>
-          <Button small to={'/lobby'}>
-            {fee ? `Buy-in ${fee}$` : 'Join for free'}
-          </Button>
-        </span>
-      </div>
-    </>
   );
 }
