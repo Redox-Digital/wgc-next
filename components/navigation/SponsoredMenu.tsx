06f384c4ad3314d88e7921cb9431ed11fa025e63
Mikaël Ruffieux <mikael@redoxdigital.ch>
2024-12-03 16:44:06 +0100

    hotfix

--- components/navigation/SponsoredMenu.tsx
+++ components/navigation/SponsoredMenu.tsx
@@ -43,7 +43,7 @@ const Header = () => {
           </div>
         </div>
       </nav>
-      <MobileMenu open={menuOpen} toggleMenu={toggleMenu} />
+      {/* <MobileMenu open={menuOpen} toggleMenu={toggleMenu} /> */}
     </>
   );
 };
