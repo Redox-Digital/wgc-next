2d2b809539ff809cad3a0321ec039ddc744e67d3
Mikaël Ruffieux <mikael@redoxdigital.ch>
2025-01-30 12:10:19 +0100

    style : better Inputs

--- components/inputs/Inputs.tsx
+++ components/inputs/Inputs.tsx
@@ -3,26 +3,59 @@ import css from './Inputs.module.scss';
 type TextInputType = {
   id: string;
   label: string;
-  defaultValue?: string;
+  value?: string;
   placeholder?: string;
   className?: string;
-  width?: 'half' | '1/3' | '2/3'; // DEV : TBC
+  dark?: boolean;
+  width?: '50' | '33' | '66';
   type: 'text' | 'number' | 'date' | 'textarea' | 'file';
 };
 
-export function TextInput({ id, label, placeholder, width, className, type }: TextInputType) {
+function getInputWidth(width?: '50' | '33' | '66') {
+  if (!width) return '';
+
+  switch (width) {
+    case '50':
+      return css.w50;
+
+    case '33':
+      return css.w33;
+
+    case '66':
+      return css.w66;
+
+    default:
+      return '';
+  }
+}
+
+export function TextInput({
+  id,
+  label,
+  placeholder,
+  width,
+  className,
+  type,
+  dark,
+  value,
+}: TextInputType) {
   if (type === 'textarea') {
     return (
       <label htmlFor={id} className={`${css.input} ${className}`}>
         <span>{label}</span>
-        <textarea id={id} name={id} placeholder={placeholder} defaultChecked></textarea>
+        <textarea id={id} name={id} placeholder={placeholder} defaultChecked>
+          {value}
+        </textarea>
       </label>
     );
   }
   return (
-    <label htmlFor={id} className={`${css.input} ${className} ${width && css.half}`}>
+    <label
+      htmlFor={id}
+      className={`${css.input} ${dark && css.dark} ${className} ${getInputWidth(width)}`}
+    >
       <span>{label}</span>
-      <input type={type} id={id} name={id} placeholder={placeholder}></input>
+      <input type={type} id={id} name={id} placeholder={placeholder} value={value}></input>
     </label>
   );
 }
@@ -30,24 +63,53 @@ export function TextInput({ id, label, placeholder, width, className, type }: Te
 type SelectInputType = {
   id: string;
   label: string;
-  half?: boolean;
-  small?: boolean;
+  value?: string;
   className?: string;
+  width?: '50' | '33' | '66';
+  dark?: boolean;
   options: { value: string; label: string }[];
 };
 
-export function SelectInput({ id, label, options, half, className, small }: SelectInputType) {
+export function SelectInput({
+  id,
+  label,
+  options,
+  className,
+  width,
+  dark,
+  value,
+}: SelectInputType) {
   return (
-    <label htmlFor={id} className={`${css.input} ${half && css.half} ${className}`}>
+    <label
+      htmlFor={id}
+      className={`${css.input} ${getInputWidth(width)} ${dark && css.dark} ${className}`}
+    >
       <span>{label}</span>
 
-      <select name={id} id={id} className={`${small && css.small}`}>
+      <select name={id} id={id}>
         {options.map((o) => (
-          <option value={o.value} key={o.value}>
+          <option value={o.value} key={o.value} selected={o.value === value}>
             {o.label}
           </option>
         ))}
       </select>
+    </label>
+  );
+}
+
+type CheckboxType = {
+  id: string;
+  checked?: boolean;
+  className?: string;
+  dark?: boolean;
+  children?: string | React.ReactNode;
+};
+
+export function Checkbox({ id, checked, className, dark, children }: CheckboxType) {
+  return (
+    <label htmlFor={id} className={`${css.checkbox} ${className} ${dark ? css.dark : ''}`}>
+      <input type="checkbox" id={id} name={id} checked={checked} />
+      <span>{children}</span>
     </label>
   );
 }
