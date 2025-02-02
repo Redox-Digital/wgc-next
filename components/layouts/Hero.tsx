df0f8f1d1f9394d491a1b287bc48fb004737300c
Mikaël Ruffieux <mikael@redoxdigital.ch>
2025-02-02 15:54:07 +0100

    feat: passing hero btns into children

--- components/layouts/Hero.tsx
+++ components/layouts/Hero.tsx
@@ -6,26 +6,14 @@ import Button from '../navigation/Button';
 type Props = {
   title: string | React.ReactNode;
   children?: string | React.ReactNode;
-  source: string;
-  home?: boolean;
-  opacity?: number;
-  btns?: { text: string; href: string; plain?: boolean }[];
 };
 
-const Hero = ({ title, children, source, home, opacity, btns }: Props) => {
+const Hero = ({ title, children }: Props) => {
   return (
     <>
       <header className={`${css.hero}`}>
         <h1>{title}</h1>
         <div className={css.content}>{children}</div>
-        <div className={css.btns}>
-          {btns &&
-            btns.map((btn) => (
-              <Button key={btn.href} href={btn.href} outline={!btn.plain} darkBg>
-                {btn.text}
-              </Button>
-            ))}
-        </div>
       </header>
       <span id="intro">{/* Anchor */}</span>
     </>
