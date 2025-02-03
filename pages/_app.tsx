af66f00244f6792b83bb63c90e78456b0940fa6a
Mikaël Ruffieux <mikael@redoxdigital.ch>
2025-02-03 08:11:03 +0100

    style: hotfix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,11 @@ import Layout from '@/components/Layout';
 import type { AppProps } from 'next/app';
 import '@/styles/wgc-globals.scss';
 
-import { Inter, Lexend } from 'next/font/google';
+import { Inter, Lexend, Syne } from 'next/font/google';
 
-const inter = Inter({ weight: ['400', '500', '700'], subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] });
 const lexend = Lexend({ subsets: ['latin'] });
+const syne = Syne({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
