04560f88e86e07c973e9c3102b3c1ff0745c24ce
Mikaël Ruffieux <mikael@redoxdigital.ch>
2025-03-18 08:26:47 +0100

    style : ChallengePreview arrow changing color

--- components/content/ChallengePreview.tsx
+++ components/content/ChallengePreview.tsx
@@ -110,7 +110,13 @@ export default function ChallengePreview({
           </div>
 
           <span className={css.arrow}>
-            <Image src={'/pictograms/angle-accent-right.svg'} alt={''} width={15} height={15} />
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 6 10">
+              <g>
+                <g id="Layer_1">
+                  <path d="M.9.9l4.1,4.1L.9,9.1" />
+                </g>
+              </g>
+            </svg>
           </span>
         </summary>
 
