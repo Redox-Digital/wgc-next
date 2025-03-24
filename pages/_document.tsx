008a400d27f5157f7fc7effcd9b0fec0e84d1e01
Mikaël Ruffieux <mikael@redoxdigital.ch>
2025-03-24 09:16:53 +0100

    content: favicon variants + social opengraph image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,16 @@ export default function Document() {
     <Html lang="fr">
       <Head>
         <link rel="icon" href="/logos/favicon.svg" type="image/svg+xml" />
+
+        <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="WGC.GG" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+
+        <meta property="og:image" content="/favicon/social_thumbnail.png" />
+        <meta name="twitter:image" content="/favicon/social_thumbnail.png" />
       </Head>
       <body className="wgc">
         {/* DEV : ajouter la class "excr" pour les pages sponsorisées */}
