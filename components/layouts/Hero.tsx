591c8488b97739f7a26cae96abe8449734d0c254
Mikaël Ruffieux <mikael@redoxdigital.ch>
2026-03-18 15:57:23 +0100

    UGxBG : new temp Hero Section

--- components/layouts/Hero.tsx
+++ components/layouts/Hero.tsx
@@ -1,4 +1,6 @@
+import Button from '../navigation/Button';
 import css from './Hero.module.scss';
+import Image from 'next/image';
 
 type Props = {
   title: string | React.ReactNode;
@@ -6,7 +8,7 @@ type Props = {
   children?: string | React.ReactNode;
 };
 
-const Hero = ({ title, children, gradient }: Props) => {
+export default function Hero({ title, children, gradient }: Props) {
   return (
     <>
       <header className={`${css.hero} ${gradient && css.gradientBg}`}>
@@ -17,6 +19,55 @@ const Hero = ({ title, children, gradient }: Props) => {
       <span id="intro">{/* Anchor */}</span>
     </>
   );
-};
+}
 
-export default Hero;
+export function EventHero() {
+  return (
+    <>
+      <header className={css.eventHero}>
+        <div className={css.container}>
+          <Image
+            className={css.mainSponsor}
+            // DEV : update file to svg
+            src="/sponsors/UGOLFxBLUEGREEN/UGOLFxBLUEGREEN_white.png"
+            alt="BLUEGREEN & UGOLF"
+            width={300}
+            height={90}
+          />
+          <div className={css.content}>
+            <div className={css.texts}>
+              <h1>Play golf, win big</h1>
+              <p>
+                Join the exclusive <b>Jaquet Droz Challenge</b> to win a unique timepiece worth
+                north of <b>€300’000.-</b> and <b>LIV Golf Pro-Am spots</b>.
+              </p>
+            </div>
+            <div className={css.actions}>
+              <Button href="/ugolf-bluegreen">Join the Challenge</Button>
+
+              <div className={css.secondarySponsor}>
+                <small>sponsored by</small>{' '}
+                <Image
+                  src="/sponsors/UGOLFxBLUEGREEN/JDx4A_white.svg"
+                  alt="Jaquet Droz x 4Aces GC"
+                  width={176}
+                  height={32}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Image
+          src="/sponsors/UGOLFxBLUEGREEN/JD_tourbillon_square.png"
+          alt=""
+          width={550}
+          height={550}
+          className={css.sideImg}
+        />
+      </header>
+
+      <span id="intro">{/* Anchor */}</span>
+    </>
+  );
+}
