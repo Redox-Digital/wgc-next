1ef841c62b663cb306782845feb59c0339b7c2dc
Mikaël Ruffieux <mikael@redoxdigital.ch>
2026-03-19 17:36:02 +0100

    feat: extending PriceList to support card display

--- components/layouts/PriceList.tsx
+++ components/layouts/PriceList.tsx
@@ -39,7 +39,7 @@ export const prizes: PrizeType[] = [
   },
   {
     title: 'Pack Officiel 4Aces',
-    logo: '/sponsors/JaquetDroz/4Aces_Logo_white.svg',
+    logo: '/sponsors/JaquetDroz/logo-under-armor-white.svg',
 
     description: (
       <>
@@ -49,6 +49,51 @@ export const prizes: PrizeType[] = [
     ),
     badge: {
       title: 'top 20 of each category',
+      className: leagueCss.tournamentReady,
+    },
+    img: '/sponsors/JaquetDroz/UGxBG_Apr2026/WGC_UGxBG_Price03.webp',
+  },
+];
+
+export const prizesShort: PrizeType[] = [
+  {
+    title: "Une œuvre d'art au poignet",
+    logo: '/sponsors/JaquetDroz/JD_white.svg',
+    description: (
+      <>
+        Tentez de remporter par tirage au sort une pièce de haute horlogerie suisse Jaquet Droz,
+        d’une valeur de € 300’000.-.
+      </>
+    ),
+    badge: {
+      title: 'tirage au sort global',
+    },
+    img: '/sponsors/JaquetDroz/UGxBG_Apr2026/WGC_UGxBG_Price01.png',
+    imgContain: true,
+  },
+  {
+    title: 'LIV Pro-Am Valderrama',
+    logo: '/sponsors/JaquetDroz/4Aces_Logo_white.svg',
+
+    description: (
+      <>
+        Marquez l'histoire en gagnant votre invitation pour jouer aux côtés des légendes lors d'un
+        Pro-Am exclusif.
+      </>
+    ),
+    badge: {
+      title: 'tirage au sort par catégorie',
+      className: leagueCss.clubhouseKing,
+    },
+    img: '/sponsors/JaquetDroz/UGxBG_Apr2026/WGC_UGxBG_Price02.jpg',
+  },
+  {
+    title: 'Le kit complet des 4Aces',
+    logo: '/sponsors/JaquetDroz/logo-under-armor-white.svg',
+
+    description: <>Pour un équipement de la tête aux pieds de notre partenaire 4Aces.</>,
+    badge: {
+      title: 'top 20 de chaque catégorie',
       className: leagueCss.tournamentReady,
     },
     img: '/sponsors/JaquetDroz/UGxBG_Apr2026/WGC_UGxBG_Price03.webp',
@@ -73,6 +118,30 @@ export default function PriceList({ lightBg }: Props) {
       <ul className={css.list}>
         {prizes.map((prize, key) => (
           <PrizeLine key={key} {...prize} />
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export function PriceListShort({ lightBg }: Props) {
+  return (
+    <section
+      className={`${css.section} ${css.shortListSection} ${lightBg && css.light}`}
+      id="pricepool"
+    >
+      <div className={css.sctTitle}>
+        <h2>Votre inscription : le ticket pour une expérience exclusive</h2>
+        <p>
+          Tentez de remporter par tirage au sort une pièce de haute horlogerie Jaquet Droz, une
+          œuvre d'art à six chiffres. Pour chaque catégorie, tentez également votre chance de
+          marquer l'histoire en gagnant votre invitation pour jouer aux côtés des légendes lors du
+          LIV Pro-Am de Valderrama.
+        </p>
+      </div>
+      <ul className={css.shortList}>
+        {prizesShort.map((prize, key) => (
+          <PrizeCard key={key} {...prize} />
         ))}
       </ul>
     </section>
@@ -127,6 +196,46 @@ export function PrizeLine({
           alt=""
           width={270}
           height={240}
+        />
+      )}
+    </li>
+  );
+}
+
+export function PrizeCard({
+  logo,
+  title,
+  description,
+  img,
+  imgContain,
+  badge,
+  className,
+}: PrizeType) {
+  return (
+    <li className={`${css.prize} ${css.card} ${className}`}>
+      <div className={css.content}>
+        {logo && <Image className={css.logo} src={logo} alt="" width={80} height={32} />}
+
+        <div className={css.texts}>
+          {badge ? (
+            <span className={`${css.badge} ${badge.className || leagueCss.rookie}`}>
+              {badge.title}
+            </span>
+          ) : (
+            ''
+          )}
+          <h3>{title}</h3>
+          {description && <small>{description}</small>}
+        </div>
+      </div>
+
+      {img && (
+        <Image
+          className={`${css.image} ${imgContain && css.contain}`}
+          src={img}
+          alt=""
+          width={360}
+          height={180}
         />
       )}
     </li>
